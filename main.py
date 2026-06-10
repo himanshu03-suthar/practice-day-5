12a4e7e57d9e072b00b979787beb49019e9b4fd8
Himanshu suthar <hs7882371@gmail.com>
2026-06-11 01:22:09 +0530

    Server running on uvicorn

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def get_items(bike: str, car: str, cycle: str):
         "car": car,
         "cycle": cycle
     }
+
+@app.post("/items/{item_id}")
+def get_item(item_id: int, name: str, price: float):
+    return{
+        "item_id": item_id,
+        "name": name,
+        "price": price
+    }
